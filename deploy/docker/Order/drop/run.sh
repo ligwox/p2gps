d1c9c499da49307b47acaa774218544dd66ebd0e
Mykhailo Korobka <mykhailo.korobka@gmail.com>
2024-11-10 17:21:29 +0100

    -

--- deploy/docker/Order/drop/run.sh
+++ deploy/docker/Order/drop/run.sh
@@ -7,4 +7,11 @@ do
 done
 echo "$(date) - connected successfully"
 
-java -jar ordering-*.jar
+java -jar /usr/local/app/ordering-*.jar
+
+#Parámetros de configuración por defecto para conectar con MongoDB. 
+#No hace falta cuando se ejecute en local, pero al usar dockercompose o en producción, 
+#hay que indicar la IP del contenedor de MongoDB (mongo) y la base de datos (ordering), 
+#datos que se buscan en el fichero application.properties. 
+mongodb.host: mongo 
+mongodb.database: ordering 
